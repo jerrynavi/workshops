20489571914239380289ad5741e2dc0c53f71300
Jerry <jerryasih@gmail.com>
2022-05-26 16:30:14 +0100

    add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+import CalendarIcon from './CalendarIcon';
+import ClockIcon from './ClockIcon';
+import ShoppingCartOutline from './ShoppingCartOutline';
+
+export { CalendarIcon, ClockIcon, ShoppingCartOutline };
