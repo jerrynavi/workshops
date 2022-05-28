41d0e944d80562ac9b2b91045c2b711716b8bf0e
Jerry <jerryasih@gmail.com>
2022-05-28 04:25:44 +0100

    add more icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,17 @@
+import BrushIcon from './BrushIcon';
 import CalendarIcon from './CalendarIcon';
 import ClockIcon from './ClockIcon';
+import CodeIcon from './CodeIcon';
+import FlashIcon from './FlashIcon';
+import LayoutIcon from './LayoutIcon';
 import ShoppingCartOutline from './ShoppingCartOutline';
 
-export { CalendarIcon, ClockIcon, ShoppingCartOutline };
+export {
+  CalendarIcon,
+  ClockIcon,
+  ShoppingCartOutline,
+  FlashIcon,
+  LayoutIcon,
+  CodeIcon,
+  BrushIcon,
+};
