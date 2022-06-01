8009375426a062fb606b58bac21e2ece829bb188
Jerry <jerryasih@gmail.com>
2022-06-01 22:59:53 +0100

    add icons: Trash, Close

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,10 +3,12 @@ import BrushIcon from './BrushIcon';
 import CalendarIcon from './CalendarIcon';
 import ChevronDownIcon from './ChevronDownIcon';
 import ClockIcon from './ClockIcon';
+import CloseIcon from './CloseIcon';
 import CodeIcon from './CodeIcon';
 import FlashIcon from './FlashIcon';
 import LayoutIcon from './LayoutIcon';
 import ShoppingCartOutline from './ShoppingCartOutline';
+import TrashIcon from './TrashIcon';
 
 export {
   CalendarIcon,
@@ -18,4 +20,6 @@ export {
   BrushIcon,
   ChevronDownIcon,
   ArrowBackIcon,
+  CloseIcon,
+  TrashIcon,
 };
